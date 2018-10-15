56062901fdd5e712ed94e51d5d4de76be7f6dcfe
Marco Antônio <ma.souza.junior@gmail.com>
2018-10-15 11:02:34 -0300

    FEA : create personal aliases

--- functions/aliases.fish
+++ functions/aliases.fish
@@ -22,7 +22,7 @@ alias l='ls -CF'
     # alias sbu="cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User"
     alias g="git"
     alias v="$EDITOR -v"
-    alias vim="$EDITOR -v"
+    alias $EDITOR="$EDITOR -v"
     alias s="sublime"
     alias ci="code-insiders"
 
@@ -31,69 +31,80 @@ alias l='ls -CF'
 ################################
 
 # git related shortcut
-alias undopush="git push -f origin HEAD^:master"
-alias gd="git diff"
-alias gdc="git diff --cached"
-alias ga="git add"
-alias gca="git commit -a -m"
-alias gcm="git commit -m"
-alias gbd="git branch -D"
-alias gst="git status -sb --ignore-submodules"
-alias gm="git merge --no-ff"
-alias gpt="git push --tags"
-alias gp="git push"
-alias grs="git reset --soft"
-alias grh="git reset --hard"
-alias gb="git branch"
-alias gcob="git checkout -b"
-alias gco="git checkout"
-alias gba="git branch -a"
-alias gcp="git cherry-pick"
-alias gl="git lg"
-alias gpom="git pull origin master"
+    alias undopush="git push -f origin HEAD^:master"
+    alias gd="git diff"
+    alias gdc="git diff --cached"
+    alias ga="git add"
+    alias gca="git commit -a -m"
+    alias gcm="git commit -m"
+    alias gbd="git branch -D"
+    alias gst="git status -sb --ignore-submodules"
+    alias gm="git merge --no-ff"
+    alias gpt="git push --tags"
+    alias gp="git push"
+    alias grs="git reset --soft"
+    alias grh="git reset --hard"
+    alias gb="git branch"
+    alias gcob="git checkout -b"
+    alias gco="git checkout"
+    alias gba="git branch -a"
+    alias gcp="git cherry-pick"
+    alias gl="git log"
+    alias gpom="git pull origin master"
 # turn on coloring on grep
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
 
 # Quick check running state of a process
-alias psef="ps -ef | peco "
-alias top="top -o cpu"
-
-# Google Closure compiler shortcuts
-alias jcc='java -jar ~/tools/closure_compiler/compiler.jar'
-
-# Shortcut to Google Chrome
-# alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-
-# start local wiki
-alias wiki='python /Volumes/Document/Dropbox/Moin_wiki/wikiserver.py'
-
-# start local markdown
-alias md='gollum --host localhost --port 4566 ~/Dropbox/md_wiki'
-
-# Start GAE server
-alias goagent='python ~/tools/goagent/proxy.py'
-
-# Start synergys server
-# alias syn='synergys --config ~/.synergy.conf'
-
-alias kunload='sudo kextunload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/'
-alias kload='sudo kextload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/'
-
-alias cnpm="npm --registry=http://registry.cnpmjs.org --cache=$HOME/.npm/.cache/cnpm"
+    alias psef="ps -ef | peco "
+    alias top="top -o cpu"
 
 # from http://news.ycombinator.com/item?id=4492682
-function tree1; tree --dirsfirst -ChFLQ 1 $argv; end
-function tree2; tree --dirsfirst -ChFLQ 2 $argv; end
-function tree3; tree --dirsfirst -ChFLQ 3 $argv; end
-function tree4; tree --dirsfirst -ChFLQ 4 $argv; end
-function tree5; tree --dirsfirst -ChFLQ 5 $argv; end
-function tree6; tree --dirsfirst -ChFLQ 6 $argv; end
+    function tree1; tree --dirsfirst -ChFLQ 1 $argv; end
+    function tree2; tree --dirsfirst -ChFLQ 2 $argv; end
+    function tree3; tree --dirsfirst -ChFLQ 3 $argv; end
+    function tree4; tree --dirsfirst -ChFLQ 4 $argv; end
+    function tree5; tree --dirsfirst -ChFLQ 5 $argv; end
+    function tree6; tree --dirsfirst -ChFLQ 6 $argv; end
 
-################################
-###  I am an Idiot
-################################
-# Stuff I never really use but cannot delete either because of http://xkcd.com/530/
-alias stfu="osascript -e 'set volume output muted true'"
-alias pumpitup="osascript -e 'set volume 10'"
-alias hax="growlnotify -a 'Activity Monitor' 'System error' -m 'WTF R U DOIN'"
+# Package Manager
+    alias y="yaourt --noconfirm"
+    alias i="yaourt -Sy --noconfirm"
+    alias u="yaourt -Syu --aur --noconfirm"
+    alias r="yaourt -Rs --noconfirm"
+
+# npm Aliases
+    alias ni="npm i -g"
+    alias nr="npm r -g"
+    alias nu="npm up -g"
+
+# yarn Aliases
+    alias js="yarn"
+    alias js0="yarn init"
+    alias jsi="yarn add"
+    alias jsig="yarn add -g"
+    alias jsid="yarn add -D"
+    alias jsr="yarn remove"
+
+# make aliases
+    alias mk="make"
+
+# docker aliases
+    alias dk="docker"
+    alias dkr="docker run"
+    alias dkc="docker-compose"
+    alias dkm="docker-machine"
+    alias dkps="docker ps"
+
+# utils
+    function tid
+        # conda_env=${1:-workspace}
+        ps -ax | grep -i $1 | grep -v 0:00.00 | awk '{ print $1 }' | uniq
+    end
+    function ak
+        kill -9 (tid $1)
+    end
+    set -x ALIAS_FILE "$HOME/.config/fish/functions/aliases.fish"
+    set -x FISH_CONFIG "$HOME/.config/fish/config.fish"
+    alias ealiases="$EDITOR $ALIAS_FILE; source $ALIAS_FILE;"
+    alias efish="$EDITOR $FISH_CONFIG; source $FISH_CONFIG;"
