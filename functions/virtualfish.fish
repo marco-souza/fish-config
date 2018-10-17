1cf391afedb3f219fe7811f0352b0ddf29978eb1
Marco Antônio <ma.souza.junior@gmail.com>
2018-10-17 12:21:24 -0300

    FIX : venvs path

--- functions/virtualfish.fish
+++ functions/virtualfish.fish
@@ -1,5 +1,5 @@
 # setup envs paths
-set -x ENVS "$HOME/.virtualenvs/"
+set -x ENVS "$HOME/.virtualenvs"
 set -x VIRTUALFISH_HOME $ENVS
 
 # setup projects paths
@@ -13,4 +13,4 @@ set -x PIP_REQUIRE_VIRTUALENV 1
 # Disable `virtualenv' activate script to override prompt
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 
-# eval (python -m virtualfish compat_aliases auto_activation global_requirements projects)
+eval (python -m virtualfish compat_aliases auto_activation global_requirements projects)
